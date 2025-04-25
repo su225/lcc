0955e96f8c04a841777b63bc9ea51d0d41144c19
Suchith J N <suchithjn22@gmail.com>
2025-04-25 22:15:32 +0200

    setup basic structure for compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::Parser;
+
+/// C-compiler for learning real compiler construction
+/// and the Rust programming language at the same time
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input source code for compilation
+    input_file: String,
+
+    /// Output object file name
+    #[arg(short = 'o', long, default_value_t = String::new())]
+    output: String,
+
+    /// Stop compiler at Lexer (useful for debugging)
+    #[arg(short = 'L', long, default_value_t = false)]
+    lexer: bool,
+
+    /// Stop compiler at Parsing stage
+    /// (Runs Lexer and Parser)
+    #[arg(short = 'P', long, default_value_t = false)]
+    parser: bool,
+
+    /// Generate assembly code, but stop before emitting .S file
+    /// for the assembler and linker.
+    #[arg(short = 'C', long, default_value_t = false)]
+    codegen: bool,
+
+    /// Generate and emit .S file for assembler
+    #[arg(short = 'S', long, default_value_t = true)]
+    emit_assembly: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
