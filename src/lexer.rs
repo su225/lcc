7afdefffe970b189e9b0614de2e0549877a2dd7d
Suchith J N <suchithjn22@gmail.com>
2025-04-27 21:36:31 +0200

    reimplement lexer - except int and identifier parsing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+use std::iter::Peekable;
+use std::str::Chars;
 use once_cell::sync::Lazy;
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
@@ -27,21 +29,22 @@ static KEYWORD_STRINGS: Lazy<HashMap<KeywordIdentifier, &'static str>> = Lazy::n
     ])
 });
 
+type Radix = u8;
 
 #[derive(Debug, Eq, PartialEq)]
-pub enum TokenType {
+pub enum TokenType<'a> {
     Keyword(KeywordIdentifier),
     OpenParentheses,
     CloseParentheses,
     OpenBrace,
     CloseBrace,
     Semicolon,
-    Identifier(String),
-    IntConstant(String),
+    Identifier(&'a str),
+    IntConstant(&'a str, Radix),
     Unknown,
 }
 
-impl Display for TokenType {
+impl<'a> Display for TokenType<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match &self {
             TokenType::Keyword(k) => f.write_str(KEYWORD_STRINGS.get(k).unwrap()),
@@ -51,106 +54,117 @@ impl Display for TokenType {
             TokenType::CloseBrace => f.write_str("}"),
             TokenType::Semicolon => f.write_str(";"),
             TokenType::Identifier(x) => f.write_fmt(format_args!("identifier:{}", x)),
-            TokenType::IntConstant(x) => f.write_fmt(format_args!("int:{}", x)),
+            TokenType::IntConstant(x, radix) => f.write_fmt(format_args!("int:[{}, radix:{}]", x, radix)),
             TokenType::Unknown => f.write_str("unknown"),
         }
     }
 }
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Token {
-    token_type: TokenType,
+pub struct Token<'a> {
+    token_type: TokenType<'a>,
     line: usize,
     column: usize,
 }
 
-impl Display for Token {
+impl<'a> Display for Token<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("<line:{}, col:{}, token:{}>", self.line, self.column, self.token_type))
     }
 }
 
-struct Lexer {
-    input: String,
-    cur_pos: usize,
+struct Lexer<'a> {
+    input: &'a str,
+    char_stream: Peekable<Chars<'a>>,
     cur_line: usize,
     cur_col: usize,
 }
 
-impl Lexer {
-    fn new(input: String) -> Self {
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Self {
         Self {
             input,
-            cur_pos: 0,
+            char_stream: input.chars().peekable(),
             cur_line: 1,
             cur_col: 1,
         }
     }
-    
-    fn make_token_at_current_position(&self, token_type: TokenType) -> Token {
-        Token {
+
+    fn tokenize_single_char(&mut self, token_type: TokenType<'a>) -> Token<'a>  {
+        let token = Token {
             token_type,
             line: self.cur_line,
             column: self.cur_col,
-        }
+        };
+        self.char_stream.next();
+        self.cur_col += 1;
+        token
     }
 
-    fn next_token(&mut self) -> Option<Token> {
-        let input = &self.input[self.cur_pos..];
-        for (i, ch) in input.char_indices() {
-            self.cur_pos += ch.len_utf8();
-            match ch {
+    fn tokenize_integer_constant(&mut self) -> Token<'a> {
+        todo!("tokenize integer constant with radix")
+    }
+
+    fn next_token(&mut self) -> Option<Token<'a>> {
+        loop {
+            let cur = self.char_stream.peek();
+            if cur.is_none() {
+                // We have reached the end of the stream. Hence, we cannot
+                // tokenize anymore. So we just return None
+                return None;
+            }
+            let cur_char = cur.unwrap();
+            match *cur_char {
+                ';' => {
+                    let token = self.tokenize_single_char(TokenType::Semicolon);
+                    return Some(token);
+                },
                 '(' => {
-                    let result = self.make_token_at_current_position(TokenType::OpenParentheses);
-                    self.cur_col += 1;
-                    return Some(result);
+                    let token = self.tokenize_single_char(TokenType::OpenParentheses);
+                    return Some(token);
                 },
                 ')' => {
-                    let result = self.make_token_at_current_position(TokenType::CloseParentheses);
-                    self.cur_col += 1;
-                    return Some(result);
+                    let token = self.tokenize_single_char(TokenType::CloseParentheses);
+                    return Some(token);
                 },
                 '{' => {
-                    let result = self.make_token_at_current_position(TokenType::OpenBrace);
-                    self.cur_col += 1;
-                    return Some(result);
+                    let token = self.tokenize_single_char(TokenType::OpenBrace);
+                    return Some(token);
                 },
                 '}' => {
-                    let result = self.make_token_at_current_position(TokenType::CloseBrace);
-                    self.cur_col += 1;
-                    return Some(result);
+                    let token = self.tokenize_single_char(TokenType::CloseBrace);
+                    return Some(token);
                 },
-                ';' => {
-                    let result = self.make_token_at_current_position(TokenType::Semicolon);
-                    self.cur_col += 1;
-                    return Some(result);
+                '0'..'9' => {
+                    let token = self.tokenize_integer_constant();
+                    return Some(token);
                 },
                 '\n' => {
                     self.cur_line += 1;
                     self.cur_col = 1;
-                    continue
+                    self.char_stream.next();
+                    continue;
                 },
                 '\t' => {
                     self.cur_col = ((self.cur_col + 7) / 8) * 8;
-                    continue
+                    self.char_stream.next();
+                    continue;
                 },
                 ' ' => {
                     self.cur_col += 1;
-                    continue
-                },
+                    self.char_stream.next();
+                    continue;
+                }
                 _ => {
-                    let result = self.make_token_at_current_position(TokenType::Unknown);
-                    self.cur_col += 1;
-                    return Some(result);
+                    return Some(self.tokenize_single_char(TokenType::Unknown));
                 },
-            };
+            }
         }
-        None
     }
 }
 
-impl Iterator for Lexer {
-    type Item = Token;
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next_token()
@@ -159,11 +173,12 @@ impl Iterator for Lexer {
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashMap;
     use crate::lexer::{Lexer, Token, TokenType};
 
     #[test]
     fn test_tokenizing_open_and_close_parentheses() {
-        let source = "()".to_string();
+        let source = "()";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -174,7 +189,7 @@ mod test {
 
     #[test]
     fn test_tokenizing_open_and_close_braces() {
-        let source = "{}".to_string();
+        let source = "{}";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -185,7 +200,7 @@ mod test {
 
     #[test]
     fn test_tokenizing_with_newlines() {
-        let source = "(\n\n)".to_string();
+        let source = "(\n\n)";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -196,7 +211,7 @@ mod test {
 
     #[test]
     fn test_tokenizing_with_tabs() {
-        let source = "(\t)".to_string();
+        let source = "(\t)";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -207,7 +222,7 @@ mod test {
 
     #[test]
     fn test_tokenizing_with_whitespaces() {
-        let source = "(   )".to_string();
+        let source = "(   )";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -218,7 +233,7 @@ mod test {
 
     #[test]
     fn test_tokenizing_semicolon() {
-        let source = ";".to_string();
+        let source = ";";
         let lexer = Lexer::new(source);
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
         assert_eq!(tokens, vec![
@@ -238,10 +253,10 @@ mod test {
             "_123_456",
         ];
         for src in identifiers.into_iter() {
-            let lexer = Lexer::new(src.to_string());
+            let lexer = Lexer::new(src);
             let tokens = lexer.into_iter().collect::<Vec<Token>>();
             let expected_tokens = vec![
-                Token { token_type: TokenType::Identifier(src.to_string()), line: 1, column: 1 }
+                Token { token_type: TokenType::Identifier(src), line: 1, column: 1 }
             ];
             assert_eq!(tokens, expected_tokens,
                        "lexing identifier {}: expected: {:?}, actual:{:?}",
@@ -251,23 +266,56 @@ mod test {
 
     #[test]
     fn test_tokenizing_valid_integers() {
-        let integers = vec![
+        let integers_base10 = vec![
             "1",
             "0",
-            "100",
+            "2",
+            "3",
+            "44",
+            "55",
+            "66",
+            "777",
+            "888",
+            "9189",
             "189087931798698368761873",
-            "0xdeadbeef",
-            "-1000",
         ];
-        for src in integers.into_iter() {
-            let lexer = Lexer::new(src.to_string());
-            let tokens = lexer.into_iter().collect::<Vec<Token>>();
-            let expected_tokens = vec![
-                Token { token_type: TokenType::IntConstant(src.to_string()), line: 1, column: 1 }
-            ];
-            assert_eq!(tokens, expected_tokens,
-                       "lexing identifier {}: expected: {:?}, actual:{:?}",
-                       src, expected_tokens, tokens);
+
+        let integers_hex = vec!["0xdeadbeef",
+            "0Xcafebabe",
+            "0XDEADBEEF",
+            "0xCAFEbabe",
+        ];
+
+        let integers_octal = vec![
+            "000",
+            "01234567",
+            "012300",
+        ];
+
+        let integers_binary = vec![
+            "0b01010010010",
+            "0b001001001",
+        ];
+
+        let int_tests = HashMap::from([
+            (10, integers_base10),
+            (16, integers_hex),
+            (8, integers_octal),
+            (2, integers_binary),
+        ]);
+
+
+        for (base, srcs) in int_tests.into_iter() {
+            for src in srcs.into_iter() {
+                let lexer = Lexer::new(src);
+                let tokens = lexer.into_iter().collect::<Vec<Token>>();
+                let expected_tokens = vec![
+                    Token { token_type: TokenType::IntConstant(src, base), line: 1, column: 1 }
+                ];
+                assert_eq!(tokens, expected_tokens,
+                           "lexing identifier {}: expected: {:?}, actual:{:?}",
+                           src, expected_tokens, tokens);
+            }
         }
     }
 }
