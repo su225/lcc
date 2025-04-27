73edf710f091d0706da28874f4ed600de84f0d1c
Suchith J N <suchithjn22@gmail.com>
2025-04-27 10:04:53 +0200

    add lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,32 @@
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use once_cell::sync::Lazy;
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Hash)]
 pub enum KeywordIdentifier {
     TypeInt,
     TypeVoid,
     Return,
 }
+
+static KEYWORDS: Lazy<HashMap<&'static str, KeywordIdentifier>> = Lazy::new(|| {
+    HashMap::from([
+        ("int", KeywordIdentifier::TypeInt),
+        ("void", KeywordIdentifier::TypeVoid),
+
+        ("return", KeywordIdentifier::Return),
+    ])
+});
+
+static KEYWORD_STRINGS: Lazy<HashMap<KeywordIdentifier, &'static str>> = Lazy::new(|| {
+    HashMap::from([
+        (KeywordIdentifier::TypeInt, "int"),
+        (KeywordIdentifier::TypeVoid, "void"),
+
+        (KeywordIdentifier::Return, "return"),
+    ])
+});
+
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum TokenType {
@@ -19,11 +41,33 @@ pub enum TokenType {
     Unknown,
 }
 
+impl Display for TokenType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            TokenType::Keyword(k) => f.write_str(KEYWORD_STRINGS.get(k).unwrap()),
+            TokenType::OpenParentheses => f.write_str("("),
+            TokenType::CloseParentheses => f.write_str(")"),
+            TokenType::OpenBrace => f.write_str("{"),
+            TokenType::CloseBrace => f.write_str("}"),
+            TokenType::Semicolon => f.write_str(";"),
+            TokenType::Identifier(x) => f.write_fmt(format_args!("identifier:{}", x)),
+            TokenType::IntConstant(x) => f.write_fmt(format_args!("int:{}", x)),
+            TokenType::Unknown => f.write_str("unknown"),
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct Token {
     token_type: TokenType,
     line: usize,
     column: usize,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("<line:{}, col:{}, token:{}>", self.line, self.column, self.token_type))
+    }
 }
 
 struct Lexer {
@@ -125,7 +169,7 @@ mod test {
         assert_eq!(tokens, vec![
             Token { token_type: TokenType::OpenParentheses, line: 1, column: 1},
             Token { token_type: TokenType::CloseParentheses, line: 1, column: 2 },
-        ])
+        ]);
     }
 
     #[test]
@@ -136,7 +180,7 @@ mod test {
         assert_eq!(tokens, vec![
             Token { token_type: TokenType::OpenBrace, line: 1, column: 1 },
             Token { token_type: TokenType::CloseBrace, line: 1, column: 2 },
-        ])
+        ]);
     }
 
     #[test]
@@ -147,7 +191,7 @@ mod test {
         assert_eq!(tokens, vec![
             Token { token_type: TokenType::OpenParentheses, line: 1, column: 1 },
             Token { token_type: TokenType::CloseParentheses, line: 3, column: 1 },
-        ])
+        ]);
     }
 
     #[test]
@@ -158,7 +202,7 @@ mod test {
         assert_eq!(tokens, vec![
             Token { token_type: TokenType::OpenParentheses, line: 1, column: 1 },
             Token { token_type: TokenType::CloseParentheses, line: 1, column: 8 },
-        ])
+        ]);
     }
 
     #[test]
@@ -169,6 +213,27 @@ mod test {
         assert_eq!(tokens, vec![
             Token { token_type: TokenType::OpenParentheses, line: 1, column: 1 },
             Token { token_type: TokenType::CloseParentheses, line: 1, column: 5 },
-        ])
+        ]);
+    }
+
+    #[test]
+    fn test_tokenizing_valid_identifiers() {
+        let identifiers = vec![
+            "abcde",
+            "abcde123",
+            "hello_world_123",
+            "helloWorld123",
+            "_abcde",
+        ];
+        for src in identifiers.into_iter() {
+            let lexer = Lexer::new(src.to_string());
+            let tokens = lexer.into_iter().collect::<Vec<Token>>();
+            let expected_tokens = vec![
+                Token { token_type: TokenType::Identifier(src.to_string()), line: 1, column: 1 }
+            ];
+            assert_eq!(tokens, expected_tokens,
+                       "lexing identifier {}: expected: {:?}, actual:{:?}",
+                        src, expected_tokens, tokens);
+        }
     }
 }
