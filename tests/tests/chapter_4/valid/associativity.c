82bd6299c94b17f2bbf7ecfa102e73d47a434a35
Suchith J N <suchithjn22@gmail.com>
2025-05-15 21:30:07 +0200

    remove extra preprocessor directives

--- tests/tests/chapter_4/valid/associativity.c
+++ tests/tests/chapter_4/valid/associativity.c
@@ -1,9 +1,3 @@
-#ifdef SUPPRESS_WARNINGS
-#ifndef __clang__
-#pragma GCC diagnostic ignored "-Wparentheses"
-#endif
-#endif
-
 int main(void) {
     return 5 >= 0 > 1 <= 0;
 }
--- tests/tests/chapter_4/valid/eq_precedence.c
+++ tests/tests/chapter_4/valid/eq_precedence.c
@@ -1,9 +1,3 @@
-#ifdef SUPPRESS_WARNINGS
-#ifndef __clang__
-#pragma GCC diagnostic ignored "-Wparentheses"
-#endif
-#endif
-
 int main(void) {
     return 3 == 1 != 2;
 }
